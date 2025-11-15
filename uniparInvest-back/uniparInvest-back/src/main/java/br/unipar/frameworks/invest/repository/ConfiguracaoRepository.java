d1b769779c8ac9afef0e85bc341c424462afed61
Rafael Lucas de Miranda <rafael.lucas.miranda@hotmail.com>
2025-11-15 15:47:44 -0300

    Delete uniparInvest-back/uniparInvest-back directory

--- uniparInvest-back/uniparInvest-back/src/main/java/br/unipar/frameworks/invest/repository/ConfiguracaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unipar.frameworks.invest.repository;
-
-import br.unipar.frameworks.invest.model.Configuracao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfiguracaoRepository extends JpaRepository<Configuracao, Long> {
-}
