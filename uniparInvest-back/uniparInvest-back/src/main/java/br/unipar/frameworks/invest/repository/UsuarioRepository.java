1ea5309d8bae45c3b45cb01bec31f6becda0e8ed
Rafael Lucas de Miranda <rafael.lucas.miranda@hotmail.com>
2025-11-16 17:24:28 -0300

    Delete uniparInvest-back directory

--- uniparInvest-back/uniparInvest-back/src/main/java/br/unipar/frameworks/invest/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unipar.frameworks.invest.repository;
-
-import br.unipar.frameworks.invest.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
