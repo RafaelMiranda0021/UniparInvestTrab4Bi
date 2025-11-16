2505a0eb7087017c0c0fc7ea621e61c2e5fa20a6
Rafael Lucas de Miranda <rafael.lucas.miranda@hotmail.com>
2025-11-16 17:26:05 -0300

    Add files via upload

---
+++ uniparInvest-back/src/main/java/br/unipar/frameworks/invest/model/Acao.java
@@ -0,0 +1,20 @@
+package br.unipar.frameworks.invest.model; 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Acao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String empresa;
+    private String ticker;
+    private String descricao;
+}
